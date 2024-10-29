4d33a32a43a2f73dd90d97b3e264f2b1575a9f7f
clodman84 <cshourin@gmail.com>
2024-10-29 23:02:46 +0530

    keypad and oled work together

--- main/keypad.c
+++ main/keypad.c
@@ -1,160 +1,75 @@
-// Copyright 2022  William Henrique (william.martins@ee.ufcg.edu.br)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include "iot_button.h"
+#include <stdint.h>
+#include <string.h>
 
-/**
- * @file keypad.c
- * @author William Henrique (william.martins@ee.ufcg.edu.br)
- * @brief 4x4 Keypad library for ESP-IDF framework
- * @version 0.1
- * @date 2022-07-30
- */
-
+#include "oled_esp_lcd.h"
+#include "ulgl.h"
 #include "keypad.h"
 
-#include <memory.h>
-#include <time.h>
-#include <esp_log.h>
+#define BUTTON_NUM 16
+static button_handle_t g_btns[BUTTON_NUM] = {0};
 
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/queue.h>
+char text_buffer[64];
+uint8_t screen[1024];
 
-/** \brief Keypad mapping array*/
-const char keypad[] = { 
-    '1', '2', '3', 'A',
-    '4', '5', '6', 'B',
-    '7', '8', '9', 'C',
-    '*', '0', '#', 'D'
-};  
+#define NUMBER_OF_ROWS 4
+#define NUMBER_OF_COLUMNS 4
 
-/** \brief Keypad configuration pions*/
-static gpio_num_t _keypad_pins[8];
+#define LONG_PRESS_DURATION_MS 1500
+#define SHORT_PRESS_DURATION_MS 100
 
-/** \brief Last isr time*/
-time_t time_old_isr = 0;
+const char* keys[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] = {
+    {"1", "2", "3", "A"},
+    {"4", "5", "6", "B"},
+    {"7", "8", "9", "C"},
+    {"*", "0", "#", "D"}
+};
 
-/** \brief Pressed keys queue*/
-QueueHandle_t keypad_queue;
-
-/**
- * @brief Handle keypad click
- * @param [in]args row number
- */
-void intr_click_handler(void *args);
-
-/**
- * @brief Enable rows'pin pullup resistor, and isr. Prepares
- * keypad to read pressed row number.
- */
-void turnon_rows()
+static const char* get_btn_index(button_handle_t btn)
 {
-    for(int i = 4; i < 8; i++) /// Columns
-    {
-        gpio_set_pull_mode(_keypad_pins[i], GPIO_PULLDOWN_ONLY);
-    }
-    for(int i = 0; i < 4; i++) /// Rows
-    {
-        gpio_set_pull_mode(_keypad_pins[i], GPIO_PULLUP_ONLY);
-        gpio_intr_enable(_keypad_pins[i]);
-    }
-}
-
-/**
- * @brief Enable columns'pin pullup resistor, and disable rows isr and pullup resistor.
- * Prepares keypad to read pressed column number. 
- */
-void turnon_cols()
-{
-    for(int i = 0; i < 4; i++) /// Rows
-    {
-        gpio_intr_disable(_keypad_pins[i]);
-        gpio_set_pull_mode(_keypad_pins[i], GPIO_PULLDOWN_ONLY);
-    }
-    for(int i = 4; i < 8; i++) /// Columns
-    {
-        gpio_set_pull_mode(_keypad_pins[i], GPIO_PULLUP_ONLY);
-    }
-}
-
-esp_err_t keypad_initalize(gpio_num_t keypad_pins[8])
-{
-    memcpy(_keypad_pins, keypad_pins, 8*sizeof(gpio_num_t));
-
-    /** Maybe cause issues if try to desinstall this flag because it's global allocated 
-     * to all pins try to use gpio_isr_register instrad of gpio_install_isr_service **/
-    ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_install_isr_service(ESP_INTR_FLAG_EDGE));
-    for(int i = 0; i < 4; i++) /// Rows
-    {
-        gpio_intr_disable(keypad_pins[i]);
-        gpio_set_direction(keypad_pins[i], GPIO_MODE_INPUT);
-        gpio_set_intr_type(keypad_pins[i], GPIO_INTR_NEGEDGE);
-        ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_isr_handler_add(_keypad_pins[i], (void*)intr_click_handler, (void*)i));
-        
-    }
-    for(int i = 4; i < 8; i++)
-    {
-        gpio_set_direction(keypad_pins[i], GPIO_MODE_INPUT);
-    }
-
-    keypad_queue = xQueueCreate(5, sizeof(char));
-    if(keypad_queue == NULL)
-        return ESP_ERR_NO_MEM;
-
-    turnon_rows();
-
-    return ESP_OK;
-}
-
-void intr_click_handler(void* args)
-{
-    int index = (int)(args);
-    
-    time_t time_now_isr = time(NULL);
-    time_t time_isr = (time_now_isr - time_old_isr)*1000L;
-    
-    if(time_isr >= KEYPAD_DEBOUNCING)
-    {
-        turnon_cols();
-        for(int j = 4; j < 8; j++)
-        {
-            if(!gpio_get_level(_keypad_pins[j]))
-            {
-                xQueueSendFromISR(keypad_queue, &keypad[index*4 + j - 4], NULL);
-                break;
-            }
+    for (size_t i = 0; i < BUTTON_NUM; i++) {
+        if (btn == g_btns[i]) {
+            // Calculate row and column from the linear index
+            size_t row = i / NUMBER_OF_ROWS; // Integer division to get the row
+            size_t col = i % NUMBER_OF_COLUMNS; // Modulo operation to get the column
+            return keys[row][col];
         }
-        turnon_rows();
     }
-    time_old_isr = time_now_isr;
-    
+    return NULL; // Return NULL if the button is not found
 }
 
-char keypad_getkey()
+static void button_single_click_cb(void *arg, void *data)
 {
-    char key;
-    if(!uxQueueMessagesWaiting(keypad_queue)) /// if is empty, return teminator character
-        return '\0';
-    xQueueReceive(keypad_queue, &key, portMAX_DELAY);    
-    return key;
+    strcat(text_buffer, get_btn_index((button_handle_t)arg));
+    draw_text(text_buffer, 0, screen);
+    display_bitmap((esp_lcd_panel_handle_t)data, screen);
 }
 
-void keypad_delete()
-{
-    for(int i = 0; i < 8; i++)
-    {   
-        gpio_isr_handler_remove(_keypad_pins[i]);
-        gpio_set_direction(_keypad_pins[i], GPIO_MODE_DISABLE);
+
+void initialize_keypad() {
+    // Define the GPIO pins for the keypad rows and columns
+    esp_lcd_panel_handle_t panel = initialise_oled();
+
+    int32_t row_gpio[NUMBER_OF_ROWS] = {19, 18, 5, 17}; // Example GPIOs for rows
+    int32_t col_gpio[NUMBER_OF_COLUMNS] = {16, 4, 2, 15}; // Example GPIOs for columns
+
+    button_config_t cfg = {
+        .type = BUTTON_TYPE_MATRIX,
+        .long_press_time = LONG_PRESS_DURATION_MS,
+        .short_press_time = SHORT_PRESS_DURATION_MS,
+        .matrix_button_config = {
+            .row_gpio_num = 0,
+            .col_gpio_num = 0,
+        }
+    };
+
+    for (int i = 0; i < NUMBER_OF_ROWS; i++) {
+        cfg.matrix_button_config.row_gpio_num = row_gpio[i];
+        for (int j = 0; j < NUMBER_OF_COLUMNS; j++) {
+            cfg.matrix_button_config.col_gpio_num = col_gpio[j];
+            g_btns[i * NUMBER_OF_ROWS + j] = iot_button_create(&cfg);
+            iot_button_register_cb(g_btns[i * NUMBER_OF_ROWS + j], BUTTON_SINGLE_CLICK, button_single_click_cb, panel);
+       }
     }
-    vQueueDelete(keypad_queue);
 }
+
